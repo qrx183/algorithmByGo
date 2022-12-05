d19841c19ff6e7c5ac92ddf44ba2155cb816c7ae
qrx183 <1330342818@qq.com>
2022-12-05 21:24:58 +0800

    12.5

---
+++ myAlgorithm/12-5.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//无重复的最长字符串
+func lengthOfLongestSubstring(s string) int {
+	res := 0
+	cacheMap := map[uint8]uint8{}
+	l := 0
+	r := 0
+
+	n := len(s)
+	for l < n {
+		if l != 0 {
+			delete(cacheMap, s[l-1])
+		}
+		ok := true
+		if r < n {
+			_, ok = cacheMap[s[r]]
+		}
+		for r < n && !ok {
+			res = int(math.Max(float64(res), float64(r-l+1)))
+			cacheMap[s[r]] = s[r]
+			r++
+			if r < n {
+				_, ok = cacheMap[s[r]]
+			}
+
+		}
+		l++
+	}
+	return res
+}
+func main() {
+	fmt.Println(lengthOfLongestSubstring("anna"))
+}
